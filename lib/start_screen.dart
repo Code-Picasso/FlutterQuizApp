c0e3337ef5da0bed90ff04b47629d1f74bb9612f
Tish <thedcodepicasso@gmail.com>
2023-05-03 07:29:10 +0100

    created start screen component

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -8,18 +8,23 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: Color.fromARGB(40, 85, 0, 150),
+          ),
           const SizedBox(height: 80),
           const Text(
-            'Ready to rack your brain?',
+            'Ready to Rack your brain?',
             style: TextStyle(
                 color: Color.fromARGB(255, 255, 255, 255), fontSize: 30),
           ),
           const SizedBox(height: 30),
-          OutlinedButton(
+          OutlinedButton.icon(
               onPressed: () {},
+              icon: Icon(Icons.arrow_circle_right),
               style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-              child: const Text('Start'))
+              label: const Text('Start'))
         ],
       ),
     );
