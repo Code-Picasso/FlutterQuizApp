846e9b849d3dd2e36f77e3bf43bede51fc007413
Tish <thedcodepicasso@gmail.com>
2023-05-03 07:30:03 +0100

    initialized quiz component

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizza/start_screen.dart';
+
+import 'package:quizza/quiz.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 8, 85, 67),
-            Color.fromARGB(198, 122, 16, 51)
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-        ),
-        child: StartScreen(),
-      ),
-    ),
-  ));
+  runApp(const Quiz());
 }
