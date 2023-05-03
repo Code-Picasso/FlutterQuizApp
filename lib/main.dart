2e91c277ad781109f6ebddc846dae2a8f02f2062
Tish <thedcodepicasso@gmail.com>
2023-05-03 02:46:35 +0100

    added image and modified text

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:quizza/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(body: StartScreen()),
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 8, 85, 67),
+            Color.fromARGB(198, 122, 16, 51)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ),
+        child: StartScreen(),
+      ),
+    ),
   ));
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -4,6 +4,24 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
   @override
   Widget build(context) {
-    return const Text('Start Screen');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+          const SizedBox(height: 80),
+          const Text(
+            'Ready to rack your brain?',
+            style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255), fontSize: 30),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              child: const Text('Start'))
+        ],
+      ),
+    );
   }
 }
